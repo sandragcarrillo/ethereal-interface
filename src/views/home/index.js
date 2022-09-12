22750e98678342337bb8a739b1e78c2e303c1579
Sandra Carrillo <sancarrillo411@gmail.com>
2022-09-12 13:08:45 -0500

    More info for the Home

--- src/views/home/index.js
+++ src/views/home/index.js
@@ -1,8 +1,9 @@
-import{Stack,Flex,Heading,Text,Button,Image,Badge,useToast, Box}from "@chakra-ui/react";
+import{Stack,Flex,Heading,Text,Button,Image,Badge,useToast, Box,Link }from "@chakra-ui/react";
 //import{Link}from "react-router-dom";
 import{useWeb3React}from "@web3-react/core";
 import{useCallback,useEffect,useState}from "react";
 import useNFTs from "../../hooks/useNFTs";
+import { FaLinkedin, FaGithub } from "react-icons/fa";
 
 
 const Home=()=>{
@@ -98,25 +99,113 @@ const Home=()=>{
 
   </Flex>
 
-  <Box bg={"#260780"}  mx="auto" minW={"100vh"}
-        width="100%">
-    <Flex>
-    <Box>
-        <Heading>
+  <Box bg={"#260780"}  
+        width={"100%" }borderRadius={"10"}>
+    <Flex direction={{base:"column",md:"row"}} spacing={{base:5,md:10}} justify={"center"}
+  align={"center"} >
+    <Box margin={"25px"}>
+        <Heading color={"white"} fontSize="5xl">
         ¿Cómo funciona?
         </Heading>
+        <Box color={"white"} margin={"5px"} fontSize="lg">
+          Al mintear tu NFT, podrás ingresar a la galería de conocimiento en audio, además tendrás el poder de decidir las categorías que serán añadidas 
+        </Box>
     </Box>
-    <Image src="https://raw.githubusercontent.com/vickiavola/proyecto-ethereum/40ac47c294dc0ae97f5fdb0d0524f28eeed2cf41/assets/imgs/cellular.svg">
+    <Image src="https://raw.githubusercontent.com/vickiavola/proyecto-ethereum/40ac47c294dc0ae97f5fdb0d0524f28eeed2cf41/assets/imgs/cellular.svg" width={"400px"}>
 
         </Image>
       </Flex>
   </Box>
    
-     
+  <Box 
+        width={"100%" }borderRadius={"10"} marginTop="20px" border={"1px"} borderColor="#260780">
+    <Flex direction={{base:"column",md:"row"}} spacing={{base:5,md:10}} justify={"center"}
+  align={"center"} >
+     <Image src="https://www.creativefabrica.com/wp-content/uploads/2021/12/03/Education-NFT-Audio-Course-Icon-Graphics-21155552-2-580x387.png" width={"500px"} borderRadius="10px">
 
+</Image>
+    <Box margin={"25px"}>
+        <Heading color={"#260780"} fontSize="5xl">
+        Nuestro objetivo
+        </Heading>
+        <Box color={"#260780"} margin={"5px"} fontSize="lg">
+         Ampliar el acceso al conocimiento descentralizado e incensurable gracias a la tecnología Blockchain. Siendo la comunidad quienes protagonizan la toma de decisión de la plataforma
+        </Box>
+    </Box>
+   
+      </Flex>
   </Box>
 
 
+<Box bg={"white"}  
+      width={"100%" }borderRadius={"10"} padding="10px">
+        <Heading color={"#260780"} m="40px" align="center">
+      Equipo
+      </Heading>
+  <Flex direction={{base:"column",md:"row"}} spacing={{base:5,md:10}} justify={"center"}
+align={"center"} >
+  <Box margin={"25px"}>
+      
+      <Box color={"#260780"} margin={"5px"} align="center">
+      <Image
+      borderRadius='md'
+      boxSize='200px'
+      src='https://pbs.twimg.com/profile_images/1462822525231054856/ByM9Sxn6_400x400.jpg'
+      alt='Sandra Carrillo'
+      />
+        <Text fontWeight="bold" margin={"2px"}>Sandra Carrillo</Text>
+        <Text>
+          Frontend developer, Jr Solidity Developer, Especialista en creación de contenido E-Learning sobre tecnología
+        </Text>
+        <Flex justify={"center"}
+  align={"center"} gap={6} marginTop="10px"> <Link href="https://github.com/sandragcarrillo" ><FaGithub w={5} h={6}></FaGithub> </Link>
+        <Link href="https://www.linkedin.com/in/sandra-carrillo" ><FaLinkedin w={5} h={6}></FaLinkedin></Link></Flex>
+      </Box>
+  </Box>
+      <Box color={"#260780"} margin={"5px"} align="center" >
+      <Image
+      borderRadius='md'
+      boxSize='200px'
+      src='https://pbs.twimg.com/profile_images/1490399448832299025/xuFkuXHi_400x400.jpg'
+      alt='Carla Martínez'
+      />
+        <Text fontWeight="bold" margin={"2px"}>Carla Martínez</Text>
+        <Text>
+        Frontend developer, Jr Solidity Developer, Especialista en creación de contenido E-Learning para conectar talentos con oportunidades
+        </Text>
+        <Flex justify={"center"}
+  align={"center"} gap={6} marginTop="10px"> <Link href="https://github.com/carlaupgrade" ><FaGithub w={5} h={6}></FaGithub> </Link>
+        <Link href="https://www.linkedin.com/in/carlamar/" ><FaLinkedin w={5} h={6}></FaLinkedin></Link></Flex>
+      </Box>
+      <Box color={"#260780"} margin={"5px"} align="center">
+      <Image
+      borderRadius='md'
+      boxSize='200px'
+      src='https://pbs.twimg.com/profile_images/1540522912750379008/v6OojeTY_400x400.jpg'
+      alt='Vicki Avola'
+      />
+        <Text fontWeight="bold" margin={"2px"}>Vicki Avola</Text>
+        <Text>
+        Ingeniera Electricista, estudiante de desarrollo de web2.0 y web3.0. Apasionada por la tecnología y la vida conectada
+        </Text>
+        <Flex justify={"center"}
+  align={"center"} gap={6} marginTop="10px"> <Link href="https://github.com/vickiavola" ><FaGithub w={5} h={6}></FaGithub> </Link>
+        <Link href="https://www.linkedin.com/in/vicki-esther-avola-40475127/" ><FaLinkedin w={5} h={6}></FaLinkedin></Link></Flex>
+      </Box>
+
+    </Flex>
+</Box>
+ 
+   
+
+</Box>
+
+
+
+
+
+ 
+   
 
 
 
--- src/views/roadmap/index.js
+++ src/views/roadmap/index.js
@@ -1,5 +1,5 @@
 import { useWeb3React } from "@web3-react/core";
-// import { Grid } from "@chakra-ui/react";
+import { Center, Heading } from "@chakra-ui/react";
 // import { Link } from "react-router-dom";
 // import Loading from "../../components/loading";
 import RequestAccess from "../../components/request-access";
@@ -11,6 +11,7 @@ const Roadmap = () => {
 
   return (
     <>
+      <Center padding={6} color={"#260780"} ><Heading>Vota por la próxima categoría para Ethereal</Heading></Center>
       <p>Hola Mudo</p>
     </>
   );
